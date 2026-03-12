b2008d7286edcd89cf611b8b3df28e959a754c97
IT25102634 <IT25102634@my.sliit.lk>
2026-03-13 00:00:44 +0530

    spring boot prject file

---
+++ MediScheduler/src/test/java/com/medischeduler/MediSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medischeduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
